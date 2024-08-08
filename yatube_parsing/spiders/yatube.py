cd59586c02c852f9d4226d208f055c0ec1fb740e
Eugene Kolotikov <jeka.pxb@yandex.ru>
2024-08-08 17:42:35 +0300

    75000

---
+++ yatube_parsing/spiders/yatube.py
@@ -0,0 +1,23 @@
+import scrapy
+
+
+class YatubeSpider(scrapy.Spider):
+    name = "yatube"
+    allowed_domains = ["158.160.177.221"]
+    start_urls = ["http://158.160.177.221/"]
+
+    def parse(self, response):
+        for quote in response.css('div.card-body'):
+            # Для каждой найденной цитаты создаём и возвращаем словарь:
+            yield {
+                'author': quote.css('strong::text').get(),
+                'text': ' '.join(
+                    t.strip() for t in quote.css('p::text').getall()
+                ).strip(),
+                'date': quote.css('small.text-muted::text').get(),
+            }
+        next_page = response.xpath("//a[contains(., 'Следующая')]/@href").get()
+        if next_page is not None:
+            # Если ссылка нашлась, загружаем страницу по ссылке
+            # и вызываем метод parse() ещё раз.
+            yield response.follow(next_page, callback=self.parse)
